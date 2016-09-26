1eb17de7e0ac30083b298e8aef9f3fa2639b216e
Doug Wright <dwright@takepart.com>
2016-09-25 19:39:42 -0700

    few config issues; working app

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+// 'use strict';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+  Image,
+  ScrollView, ListView,
+  Navigator, TouchableHighlight
 } from 'react-native';
 
-class lister extends Component {
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+class listerApp extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return React.createElement(
+      Text,
+      {style: styles.text},
+      "Yee Haw");
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('lister', () => lister);
+AppRegistry.registerComponent('lister', () => listerApp);
